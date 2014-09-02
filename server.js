af3416b972e17a82f5aca9e48366996aec348dbc
Austin Riendeau <austin@apriendeau.com>
2014-09-02 10:26:05 -0600

    buildpack

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
-
+var port = process.env.PORT || 3000
 app.get('/', function(req, res){
   res.send('hello world')
 })
 
-app.listen(3000, function (){
-    console.log('App is listening on port ' + 3000)
+app.listen(port, function (){
+    console.log('App is listening on port ' + port)
 })
