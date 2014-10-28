1f808e715843310139b1a307637354238932962e
Austin Riendeau <austin@apriendeau.com>
2014-10-27 22:10:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3535
 app.get('/', function(req, res){
+  var test = JSON.stringify({status: 200, route: '/', message: 'success'})
+  console.log(test)
   res.send('hello world')
 })
 //change
